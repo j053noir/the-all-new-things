763d4edc611cc33e825483921e0c14e2210a1d74
Jose Alejandro Rojano Crisson <j053noir@gmail.com>
2019-01-24 15:45:55 -0500

    POST create task

--- server/api/v1/tasks/controller.js
+++ server/api/v1/tasks/controller.js
@@ -1,7 +1,31 @@
 const tasks = [];
 
+function currentDate() {
+  const d = new Date();
+  return `${d.getFullYear()}-${d.getMonth()}-${d.getDate()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+}
+
 exports.create = (req, res, next) => {
-  res.json({});
+  if (req.body.description && req.body.author) {
+    const task = {
+      id: tasks.length + 1,
+      description: req.body.description,
+      author: req.body.author,
+      createdAt: currentDate(),
+      updatedAt: null,
+    };
+    tasks.push(task);
+    res.status(201);
+    res.json({
+      message: 'Task created',
+    });
+    return;
+  }
+  next({
+    message: 'Params "description" and "author" are required',
+    statusCode: 400,
+    type: 'warn',
+  });
 };
 
 exports.all = (req, res, next) => {
